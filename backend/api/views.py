27b6841950f6f5b48ac9241329e4ad0363f1bfd3
Константин Стеблев <konstantin.sks85@gmail.com>
2023-07-27 18:45:00 +0200

    1st

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 # from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import viewsets, status, exceptions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .serializers import (
     UserSerializer, TagSerializer,
     IngredientSerializer, IngredientsCreateOrUpdateSerializer,
@@ -14,7 +17,7 @@ from .permissions import IsAdminOrReadOnly, IsAdminOrAuthorOrReadOnly
 from .filters import IngredientFilter, RecipeFilter
 from users.models import User, Subscription
 from recipes.models import (Tag, Ingredient, Recipe,
-                            AmountOfIngredients)
+                            AmountOfIngredients, Favorite, ShoppingList)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -36,7 +39,7 @@ class IngredientViewSet(viewsets.ModelViewSet):
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
-    queryset = Recipe.objects.all()
+    queryset = Recipe.objects.all().order_by('id')
     permission_classes = (IsAdminOrAuthorOrReadOnly,)
     filter_backends = (DjangoFilterBackend,)
     filterset_class = RecipeFilter
@@ -46,6 +49,62 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if self.action in ('list', 'retrieve'):
             return RecipeReadOnlySerializer
         return RecipeCreateOrUpdateSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=['post', 'delete'],
+            permission_classes=(IsAuthenticated,))
+    def favorite(self, request, pk):
+        recipe = get_object_or_404(Recipe, pk=pk)
+        user = self.request.user
+
+        if self.request.method == 'POST':
+            if Favorite.objects.filter(
+                user=user,
+                recipe=recipe
+            ).exists():
+                raise exceptions.ValidationError('Рецепт уже в избранном.')
+
+            Favorite.objects.create(user=user, recipe=recipe)
+            serializer = FavoriteRecipeSerializer(
+                recipe,
+                context={'request': request}
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if self.request.method == 'DELETE':
+            get_object_or_404(Favorite, user=user, recipe=recipe).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=True, methods=['post', 'delete'],
+            permission_classes=(IsAuthenticated,))
+    def shopping_list(self, request, pk):
+        recipe = get_object_or_404(Recipe, pk=pk)
+        user = self.request.user
+
+        if self.request.method == 'POST':
+            if ShoppingList.objects.filter(
+                user=user,
+                recipe=recipe
+            ).exists():
+                raise exceptions.ValidationError('Рецепт уже в избранном.')
+
+            ShoppingList.objects.create(user=user, recipe=recipe)
+            serializer = FavoriteRecipeSerializer(
+                recipe,
+                context={'request': request}
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        if self.request.method == 'DELETE':
+            get_object_or_404(ShoppingList, user=user, recipe=recipe).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=False, methods=['get'],
+            permission_classes=(IsAuthenticated,))
+    def download_shopping_cart(self, request, pk):
+        ...
 
 
 class AmountOfIngredientsViewSet(viewsets.ModelViewSet):
