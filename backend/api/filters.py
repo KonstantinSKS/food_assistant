c9ccb6325641b9fb3c19d1370d5a8d77544ed0c5
Константин Стеблев <konstantin.sks85@gmail.com>
2023-08-18 02:13:47 +0200

    to_test_2

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -36,12 +36,12 @@ class RecipeFilter(django_filters.FilterSet):
 
     def is_favorited_filter(self, queryset, name, value):
         user = self.request.user
-        if value and not user.is_anonymous:
+        if value and user.is_authenticated:
             return queryset.filter(favorites__user=user)
         return queryset
 
     def is_in_shopping_cart_filter(self, queryset, name, value):
         user = self.request.user
-        if value and not user.is_anonymous:
+        if value and user.is_authenticated:
             return queryset.filter(shoppinglist__user=user)
         return queryset
